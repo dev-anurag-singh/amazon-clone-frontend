cc9ab2506f960ba5a2ca1c5aa0a4ec0f21d60b4c
Anurag <dev.anuragkr@gmail.com>
2021-09-22 13:47:43 +0530

    initial commit

--- src/apis/products.js
+++ src/apis/products.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://localhost:8000/api/v1/products',
+  baseURL: `${process.env.REACT_APP_SERVER_URL}/api/v1/products`,
 });
